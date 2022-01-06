7b5701770bdc2e8729180f5721f2211f69bfa6cd
Kozak24 <okozak24@gmail.com>
2022-01-06 23:32:40 +0200

    Finalized Prototype pattern

--- creational/prototype/logic/soldier.py
+++ creational/prototype/logic/soldier.py
@@ -1,15 +1,59 @@
+from __future__ import annotations
+import copy
+
 from creational.prototype.data import Fraction
 
 
 class Soldier:
-    _ID = 0
+    _ID_COUNT = 0
+    _MANUAL_DEEP_COPY_ATTRS = ("_fraction", "_id")
 
     def __init__(self, hp: int, attack: int, fraction: Fraction):
-        self._id = Soldier._ID
+        self._id = Soldier._ID_COUNT
+        self.hp = hp
+        self.attack = attack
+        self.fraction = fraction
+        Soldier._ID_COUNT += 1
+
+    @property
+    def hp(self) -> int:
+        return self._hp
+
+    @hp.setter
+    def hp(self, hp: int):
         self._hp = hp
+
+    @property
+    def attack(self) -> int:
+        return self._attack
+
+    @attack.setter
+    def attack(self, attack: int):
         self._attack = attack
+
+    @property
+    def fraction(self) -> int:
+        return self._fraction
+
+    @fraction.setter
+    def fraction(self, fraction: int):
         self._fraction = fraction
-        Soldier._ID += 1
+
+    def clone(self) -> Soldier:
+        return self.__deepcopy__(self.__dict__)
+
+    def __deepcopy__(self, memodict={}) -> Soldier:
+        instance = Soldier.__new__(Soldier)
+        instance._id = Soldier._ID_COUNT + 1
+        instance.fraction = self.fraction
+
+        for attr_name, value in self.__dict__.items():
+            if attr_name not in self._MANUAL_DEEP_COPY_ATTRS:
+                setattr(instance, attr_name, copy.deepcopy(value, memodict))
+
+        Soldier._ID_COUNT += 1
+
+        return instance
 
     def __str__(self) -> str:
-        return f"Soldier with ID '{self._id}'"
+        return f"Soldier with ID '{self._id}', HP '{self.hp}' and Attack '{self.attack}' from {self.fraction}"
--- creational/prototype/prototype_main.py
+++ creational/prototype/prototype_main.py
@@ -1,9 +1,38 @@
 from creational.prototype.data import Fraction
+from creational.prototype.logic import Soldier
+
+import random
 
 
 def main():
     blue_fraction = Fraction("Blue")
     red_fraction = Fraction("Red")
+
+    blue_soldier_proto = Soldier(100, 20, blue_fraction)
+    red_soldier_proto = Soldier(100, 20, red_fraction)
+
+    blue_army = create_army(blue_soldier_proto, 10)
+    red_army = create_army(red_soldier_proto, 9)
+
+    for blue_soldier in blue_army:
+        print(blue_soldier)
+
+    print("\n")
+
+    for red_soldier in red_army:
+        print(red_soldier)
+
+
+def create_army(soldier_proto: Soldier, soldiers_amount: int = 50):
+    soldiers = []
+
+    for soldier_number in range(0, soldiers_amount):
+        soldier_clone = soldier_proto.clone()
+        soldier_clone.hp = random.randint(20, 26)
+        soldier_clone.attack = random.randint(95, 125)
+        soldiers.append(soldier_clone)
+
+    return soldiers
 
 
 if __name__ == "__main__":
