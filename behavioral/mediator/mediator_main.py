5035fbdafed485f0fb0a25fca985f823d5f1afef
Kozak24 <okozak24@gmail.com>
2022-03-26 14:15:01 +0200

    Minor refactoring of mediator_main

--- behavioral/mediator/mediator_main.py
+++ behavioral/mediator/mediator_main.py
@@ -9,10 +9,7 @@ def main() -> None:
     alarm_mediator = AlarmMediator()
     air_alarm_service = AirAlarmService(alarm_mediator)
 
-    andrew = Person("Andrew", City.KYIV, alarm_mediator)
-    dmytro = Person("Dmytro", City.KYIV, alarm_mediator)
-    marie = Person("Marie", City.DONETSK, alarm_mediator)
-    markiian = Person("Markiian", City.ZHYTOMYR, alarm_mediator)
+    create_persons(alarm_mediator)
 
     for city in City:
         air_alarm_service.notify_air_alarm(city)
@@ -21,6 +18,14 @@ def main() -> None:
     for city in reversed(City):
         air_alarm_service.notify_air_alarm_stop(city)
         sleep(randrange(3, 7))
+
+
+def create_persons(alarm_mediator) -> None:
+    # NOTE: In the demo purposes there is no sense to keep instances of Person, since no further modification of them
+    Person("Andrew", City.KYIV, alarm_mediator)
+    Person("Dmytro", City.KYIV, alarm_mediator)
+    Person("Marie", City.DONETSK, alarm_mediator)
+    Person("Markiian", City.ZHYTOMYR, alarm_mediator)
 
 
 if __name__ == "__main__":
